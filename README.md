257cbafe76fa002cf8f2c29c93c7d05d7394b3ca
thiswillbeyourgithub <github@32mail.33mail.comm>
2021-09-08 12:40:15 +0200

    daemon

--- README.md
+++ README.md
@@ -5,10 +5,15 @@
 * Linux only, needs to have [playerctl](https://github.com/altdesktop/playerctl) installed
 * 2 modes available, either poor quality speech using `espeak`, or high quality using [TransformerTTS](https://github.com/as-ideas/TransformerTTS)
 * Contributions welcome.
-* Can be run as daemon, using `exec --no-startup-id 'start-stop-daemon --start --background -C --pidfile /tmp/spotify_tts_pid --make-pidfile --exec /Full/path/Spotify_tts/spotify_tts.py -d /Full/path/TransformerTTS`
+* My crontab is something as follow: 
+```
+*/5 * * * * start-stop-daemon  --pidfile /tmp/spotify_tts_pid --make-pidfile --background --start --exec /PATH/spotify_tts.py -d /PATH/TransformerTTS
+59 * * * * kill $(pgrep --full "spotify_tts.py")
+```
+
 
 # Usage:
 * `git clone https://github.com/thiswillbeyourgithub/Spotify_tts/`
 * `cd Spotify_tts`
 * `pip3 install -r ./requirements.txt` **optionnal, only if you want to use AI powered TTS**
-* `cd Spotify_tts && python3 ./spotify_tts.py` 
+* `cd Spotify_tts && python3 ./spotify_tts.py`  to launch it directly, otherwise run it as daemon like shown above
--- spotify_tts.py
+++ spotify_tts.py
@@ -4,16 +4,37 @@ import subprocess
 import time
 import os
 import sys
+import psutil
 from pathlib import Path
 
 # USER SETTINGS: ###############################
 High_quality_speech = True  # True for AI powered TTS, False to use espeak
-rate = 22000  # default to 24000, changes the playback speed
-step = 95000  # step of the model used, changing the value requires downloading
-# a different model each time
+rate = 22000  # default to 22000, changes the playback speed
+step = 60000  # step of the model used, changing the value requires downloading
+# a different model each time, default is 95000
 title_max_length = 50
 espeak_args = "-v en -a85 -k20 -p60 -s150 --punct=''"
 ##################################################
+
+
+# this loop makes sure not to load the AI libraries if spotify is not running
+process_list = [" ".join(x.as_dict()["cmdline"]) for x in psutil.process_iter()]
+cnt = 0
+dcnt = 0
+for p in process_list:
+    if "spotify" in p:
+        cnt += 1
+        if "tts" in p and "python" in p and "nvim" not in p:
+            dcnt += 1
+if dcnt == 1:
+    print("Only 1 daemon found, so it's me, running.")
+if dcnt > 1:
+        print("Daemon already running. Exiting.")
+        raise SystemExit()
+if cnt == 0:
+    print("Spotify is not running. Exiting.")
+    raise SystemExit()
+
 
 
 if High_quality_speech is True:
@@ -24,6 +45,7 @@ if High_quality_speech is True:
     tts_dir = "/".join(os.getcwd().split("/")[0:-1]) + "/TransformerTTS"
     os.chdir(tts_dir)
     sys.path.insert(0, tts_dir)
+    print(f"Temporarily added folder {tts_dir} to path.")
     from data.audio import Audio
     from model.factory import tts_ljspeech
     from scipy.io.wavfile import write
@@ -45,7 +67,8 @@ def run_shell_cmd(cmd):
 
 
 print("\n\nDone initializing stuff.")
-while True:
+cnt = 0
+while cnt < int(60*59):  # launched for only one hour
     is_playing = run_shell_cmd("playerctl --player spotify status")
     if "Playing" in is_playing:
         current_metadata = run_shell_cmd("playerctl --player spotify metadata")
@@ -55,7 +78,7 @@ while True:
         for line in current_metadata:
             if "xesam:title " in line:
                 title = line.split("xesam:title")[1]
-                title = title.strip().replace(".", "").replace(",", "")
+                title = title.strip().replace(".", "").replace(",", "").replace("-", ",")
                 title = title[0:title_max_length]
                 if "featuring" not in title:
                     title = title.replace("feat", "featuring")
@@ -70,7 +93,7 @@ while True:
                 if len(artist) > title_max_length:
                     artist = artist[0:title_max_length]
         if to_read_flag is True:
-            print(f"Playing: {title} by {artist}")
+            print(f"Playing: {title}, by {artist}.")
             run_shell_cmd("playerctl --player spotify pause")
 
             if High_quality_speech is False:
@@ -79,10 +102,11 @@ while True:
                 out = model.predict(f"{title}, by {artist}.")
                 wav = audio.reconstruct_waveform(out['mel'].numpy().T)
                 write("output.wav", data=wav, rate=rate)
-                time.sleep(0.2)
+                time.sleep(0.1)
                 playsound("output.wav")
-                time.sleep(0.2)
+                time.sleep(0.1)
 
             run_shell_cmd("playerctl --player spotify play")
         previous_song = title
     time.sleep(1)
+    cnt +=1
